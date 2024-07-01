8791464e007c85f3a589224e625ca850919b3ec1
dhruv8110 <111770175+dhruv8110@users.noreply.github.com>
2024-07-01 10:40:14 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript to handle the hamburger menu toggle
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('.nav-links');
+
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
